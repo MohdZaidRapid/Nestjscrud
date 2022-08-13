e41759179d2de67fd4fe7be6e6a709d32a43118c
mohdzaid <mohdzaid@gmail.com>
2022-08-13 17:10:20 +0530

    crud added

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -5,12 +5,14 @@ import {
   HttpCode,
   Param,
   Post,
+  Query,
   Redirect,
   Req,
 } from '@nestjs/common';
 import { Request } from '@nestjs/common';
-
+import { CatsService } from './cats.service';
 import { CreateCatDto } from './create.cat.dto';
+import { Cat } from './interface/cat.interface';
 
 @Controller('cats')
 // export class CatsController {
@@ -20,24 +22,25 @@ import { CreateCatDto } from './create.cat.dto';
 //   }
 // }
 export class CatsController {
+  constructor(private catsService: CatsService) {}
   @Post()
   async create(@Body() CreateCatDto: CreateCatDto) {
-    return 'This action adds a new cat';
+    this.catsService.create(CreateCatDto);
   }
 
   @Get()
-  findAll(): string {
-    return 'This action return all cats';
+  async findAll(): Promise<Cat[]> {
+    return this.catsService.findAll();
   }
 
-  @Get('ab*cd')
-  findoneapth(): string {
-    return 'This action return astrik cats';
-  }
+  //   @Get('ab*cd')
+  //   findoneapth(): string {
+  //     return 'This action return astrik cats';
+  //   }
 
-  @Get(':id')
-  findOne(@Param() params): string {
-    console.log(params.id);
-    return `this action return a #${params.id} cat`;
-  }
+  //   @Get(':id')
+  //   findOne(@Param() params): string {
+  //     console.log(params.id);
+  //     return `this action return a #${params.id} cat`;
+  //   }
 }
